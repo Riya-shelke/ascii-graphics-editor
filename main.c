53a4d670c0187ac07802302f35d5443cca7e323e
Riya Shelke <riyashelke2008@gmail.com>
2026-06-08 23:13:05 +0530

    Day 1: Created canvas and display function

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 
+#define ROWS 20
+#define COLS 40
+
+char canvas[ROWS][COLS];
+
+void initCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < ROWS; i++)
+    {
+        for(j = 0; j < COLS; j++)
+        {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    int i, j;
+
+    for(i = 0; i < ROWS; i++)
+    {
+        for(j = 0; j < COLS; j++)
+        {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
-    printf("Hello Graphics Editor!\n");
+    initCanvas();
+    displayCanvas();
+
     return 0;
 }
