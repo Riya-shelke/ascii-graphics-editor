11476eaf7ef963f2aa368f85535e14c1b4023dbb
Riya Shelke <riyashelke2008@gmail.com>
2026-06-09 11:12:49 +0530

    Day 3: Added rectangle drawing

--- main.c
+++ main.c
@@ -40,11 +40,36 @@ void drawHorizontalLine(int row, int startCol, int endCol)
         canvas[row][i] = '*';
     }
 }
+void drawVerticalLine(int col, int startRow, int endRow)
+{
+    int i;
+
+    for(i = startRow; i <= endRow; i++)
+    {
+        canvas[i][col] = '*';
+    }
+}
+void drawRectangle(int row, int col, int width, int height)
+{
+    drawHorizontalLine(row, col, col + width - 1);
+
+    drawHorizontalLine(row + height - 1,
+                       col,
+                       col + width - 1);
+
+    drawVerticalLine(col,
+                     row,
+                     row + height - 1);
+
+    drawVerticalLine(col + width - 1,
+                     row,
+                     row + height - 1);
+}
 
 int main()
 {
     initCanvas();
-    drawHorizontalLine(5, 10, 20);
+    drawRectangle(5, 10, 15, 6);
     displayCanvas();
 
     return 0;
